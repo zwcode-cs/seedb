cbbd2a0307c2a356e8185ca87adf62b435e10421
Sashko Stubailo <sashko@mit.edu>
2013-12-02 02:36:47 -0500

    Replaced bootstrap with a single file

--- SeeDB-node-frontend/assets/js/application.js
+++ SeeDB-node-frontend/assets/js/application.js
@@ -1,7 +1,7 @@
 //= require jquery-2.0.3
 //= require underscore-1.5.2
 //= require backbone-1.1.0
-//= require_tree bootstrap
+//= require bootstrap
 //= require handlebars.runtime-v1.1.2
 //= require_tree .
 
