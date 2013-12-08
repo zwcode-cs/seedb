7aa0f7ed9364201961e74e9bc5fa6915f946ec16
Sashko Stubailo <sashko@mit.edu>
2013-12-08 09:45:31 -0500

    Fixed up code lint

--- SeeDB-node-frontend/hbs_helpers.js
+++ SeeDB-node-frontend/hbs_helpers.js
@@ -1,9 +1,15 @@
-var hbs = require("express-hbs");
+(function(){
+  "use strict";
 
-hbs.registerHelper("js", function(path) {
-  return js(path);
-});
+  var hbs = require("express-hbs");
+  var js = js;
+  var css = css;
 
-hbs.registerHelper("css", function(path) {
-  return css(path);
-});
+  hbs.registerHelper("js", function(path) {
+    return js(path);
+  });
+
+  hbs.registerHelper("css", function(path) {
+    return css(path);
+  });
+}());
