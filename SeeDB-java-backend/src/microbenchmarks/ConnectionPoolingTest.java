9081765d8c1e510ad512833a23aa3f5d99bf14db
MV <mpvartak@gmail.com>
2014-02-25 16:24:45 -0500

    multiple group by

--- SeeDB-java-backend/src/microbenchmarks/ConnectionPoolingTest.java
+++ SeeDB-java-backend/src/microbenchmarks/ConnectionPoolingTest.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class ConnectionPoolingTest {
 	
 	public static void main(String[] args) {
-		ConnectionPooling cp = new ConnectionPooling(10, "postgresql", 
+		ConnectionPooling cp = new ConnectionPooling(4, "postgresql", 
 				"127.0.0.1/seedb_data", "postgres", "postgrespwd");
 		try {
 			cp.runQueriesOnMultipleConnections("table_10_2_2_3_2_1"); //"table_1m_10_10_test"); //"table_10_2_2_3_2_1");
