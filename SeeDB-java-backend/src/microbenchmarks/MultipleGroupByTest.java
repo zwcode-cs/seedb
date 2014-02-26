12bb514c6ef63cce17f382ae5d796160dfef5604
MV <mpvartak@gmail.com>
2014-02-26 10:40:43 -0500

    Minor fixes to benchmarks

--- SeeDB-java-backend/src/microbenchmarks/MultipleGroupByTest.java
+++ SeeDB-java-backend/src/microbenchmarks/MultipleGroupByTest.java
@@ -4,14 +4,21 @@ import java.sql.SQLException;
 
 public class MultipleGroupByTest {
 	public static void main(String[] args) {
-		MultipleGroupBy mgb = new MultipleGroupBy("table_1m_10_10_test", 64, // table_1m_10_10_test, table_10_2_2_3_2_1
-				"postgresql", "127.0.0.1/seedb_data", "postgres", "postgrespwd");
-		try {
-			mgb.runMultipleGroupByTest();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		//String[] tables = new String[] { "xs_1", "xs_2", "xs_3", "s_1", "s_2", "s_3" };
+		String[] tables = new String[] {"table_10_2_2_3_2_1"};
+		for (int mem = 1000; mem < 10000000; mem=mem*10) {
+			for (String table : tables) {
+				MultipleGroupBy mgb = new MultipleGroupBy(table, mem, // table_1m_10_10_test, table_10_2_2_3_2_1
+						"postgresql", "127.0.0.1/seedb_data", "postgres", "postgrespwd");
+				try {
+					mgb.runMultipleGroupByTest();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
+		
 	}
 
 }
