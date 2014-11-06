761a8e50b4dd4b960d862ad3bd29fb288dd34c49
Samuel Madden <madden@csail.mit.edu>
2014-11-06 17:30:58 -0500

    fix bin packing labels

--- sigmod_figures/create_multi_gb_chart.R
+++ sigmod_figures/create_multi_gb_chart.R
@@ -11,7 +11,7 @@ gg_color_hue <- function(n) {
   hcl(h=hues, l=65, c=100)[1:n]
 }
 
-multi_gb <- function(filename) {
+multi_gb <- function(filename,showlines) {
     dirname="";
     outdir = "../full-paper/Images/";
         tmp = read.table(paste(dirname, filename, ".txt", sep=""), sep="\t");
@@ -20,10 +20,17 @@ multi_gb <- function(filename) {
 	tmp$groups = 10^tmp$n_gb;
         tmp$dbms = factor(tmp$dbms);
 	cols = gg_color_hue(2)
-        #ggplot(tmp, aes(n_gb, latency/1000, color=dbms)) + geom_line() + geom_point() + theme(text = element_text(size=24)) + ylab("latency(s)") + geom_hline(aes(yintercept=195), color=cols[2], linetype="dashed") + geom_hline(aes(yintercept=123), color=cols[1], linetype="dashed");
+                                        #ggplot(tmp, aes(n_gb, latency/1000, color=dbms)) + geom_line() + geom_point() + theme(text = element_text(size=24)) + ylab("latency(s)") + geom_hline(aes(yintercept=195), color=cols[2], linetype="dashed") + geom_hline(aes(yintercept=123), color=cols[1], linetype="dashed");
+    if (showlines) {
+        ggplot(tmp, aes(groups, latency/1000, color=dbms))  + theme_bw() + geom_line(size=1.5) + geom_point(size=2) + theme(text = element_text(size=24)) + ylab("latency(s)") + scale_x_log10(name="Number of groups") + scale_fill_brewer(palette="Paired") + geom_hline(aes(yintercept=195), color=cols[2], linetype="dashed") + geom_hline(aes(yintercept=123), color=cols[1], linetype="dashed") +
+            geom_text(data=NULL, aes(10^22,140,label="Col. bin packing"), color=cols[1]) + geom_text(data=NULL, aes(10^22,210,label="Row. bin packing"),color=cols[2]);
+	ggsave(file=paste(outdir, filename, ".pdf", sep=""));
+    } else {
         ggplot(tmp, aes(groups, latency/1000, color=dbms))  + theme_bw() + geom_line(size=1.5) + geom_point(size=2) + theme(text = element_text(size=24)) + ylab("latency(s)") + scale_x_log10(name="Number of groups") + scale_fill_brewer(palette="Paired");
 	ggsave(file=paste(outdir, filename, ".pdf", sep=""));
+
+    }
 }
 
-multi_gb("multi_gb");
-multi_gb("multi_gb_same");
+multi_gb("multi_gb", TRUE);
+multi_gb("multi_gb_same", FALSE);
