dd651fffa6e153f2486241143105cd7c6a43c2b3
Samuel Madden <madden@csail.mit.edu>
2014-11-06 18:35:10 -0500

    figure fixes

--- sigmod_figures/create_in_memory_chart.R
+++ sigmod_figures/create_in_memory_chart.R
@@ -10,7 +10,7 @@ in_memory <- function(filename) {
 
 	ggplot(tmp[tmp$algo!='RANDOM',], aes(k, latency/1000, color=algo)) +  theme_bw() + geom_line(size=1.5) + geom_point() + 
 		ylab("latency(s)") + theme(text = element_text(size=24))  + scale_fill_brewer(palette="Paired");
-	ggsave(file=paste(outdirname, filename, "_latency.pdf", sep=""));
+	ggsave(file=paste(outdirname, filename, "_latency.pdf", sep=""),width=7,height=5);
 
 	#ggplot(tmp, aes(k, utility_dist, color=algo)) + geom_line() + geom_point() + 
 	#	ylab("utility distance") + theme(text = element_text(size=24)) + 
@@ -21,7 +21,7 @@ in_memory <- function(filename) {
 		geom_line(size=1.5) + geom_point() +  theme_bw() + 
 		ylab("utility distance") + theme(text = element_text(size=24)) +
 		scale_fill_brewer(palette="Paired");
-	ggsave(file=paste(outdirname, filename, "_utility_dist.pdf", sep=""));
+	ggsave(file=paste(outdirname, filename, "_utility_dist.pdf", sep=""),width=7,height=5);
 
 	#ggplot(tmp, aes(k, accuracy, color=algo, max=accuracy + accuracy_se, ymin=accuracy - accuracy_se)) + ylim(0, 1) + geom_line() + geom_point() + 
 	#	ylab("accuracy") + theme(text = element_text(size=24)) +
@@ -31,7 +31,7 @@ in_memory <- function(filename) {
 		ylim(0, 1.2) + geom_line(size=1.5) +  theme_bw() + geom_point() + 
  		ylab("accuracy") + theme(text = element_text(size=24)) +
  		scale_fill_brewer(palette="Paired");
-	ggsave(file=paste(outdirname, filename, "_accuracy.pdf", sep=""));
+	ggsave(file=paste(outdirname, filename, "_accuracy.pdf", sep=""),width=7,height=5);
 }
 
 in_memory("in_memory_bank");
--- sigmod_figures/create_intro_plot.R
+++ sigmod_figures/create_intro_plot.R
@@ -1,25 +1,25 @@
 library(reshape2)
 library(ggplot2)
-month = factor(c("AT&T", "Verizon", "T-Mobile", "Sprint"), order=TRUE)
+month = factor(c("AT&T", "Verizon", "T-Mob", "Sprint"), order=TRUE)
 avg_profit_StaplerX = c(180.55, 145.50, 122.00, 90.13)
 demo1 = data.frame(month, avg_profit_StaplerX)
-ggplot(data=demo1) + theme_bw() + geom_bar(aes(x=month, y=avg_profit_StaplerX), stat="identity") + 
+ggplot(data=demo1) + theme_bw() + geom_bar(aes(fill=month,x=month, y=avg_profit_StaplerX), stat="identity") + 
 	ylab("Avg. load time (ms") + xlab("Carrier") + theme(text=element_text(size=24))
-ggsave(scale=1.5, file="../full-paper/Images/dist1.pdf")
+ggsave(file="../full-paper/Images/dist1.pdf",width=7,height=5)
 dev.off()
 
 
 avg_profit_StaplerX = c(26545.80, 29390.76, 34577.11, 43789.53)
 demo1 = data.frame(month, avg_profit_StaplerX)
-ggplot(data=demo1) + theme_bw() + geom_bar(aes(x=month, y=avg_profit_StaplerX), stat="identity") + 
+ggplot(data=demo1) + theme_bw() + geom_bar(aes(fill=month,x=month, y=avg_profit_StaplerX), stat="identity") + 
 	ylab("Avg. load time (ms") + xlab("Carrier") + theme(text=element_text(size=24))
-ggsave(scale=1.5, file="../full-paper/Images/dist2.pdf")
+ggsave(file="../full-paper/Images/dist2.pdf",width=7,height=5)
 dev.off()
 
 avg_profit_StaplerX = c(43789.53, 34577.11, 29390.76, 26545.80)
 demo1 = data.frame(month, avg_profit_StaplerX)
-ggplot(data=demo1) + theme_bw() + geom_bar(aes(x=month, y=avg_profit_StaplerX), stat="identity") + 
+ggplot(data=demo1) + theme_bw() + geom_bar(aes(fill=month,x=month, y=avg_profit_StaplerX), stat="identity") + 
 	ylab("Avg. load time (ms") + xlab("Carrier") + theme(text=element_text(size=24))
-ggsave(scale=1.5, file="../full-paper/Images/dist3.pdf")
+ggsave(file="../full-paper/Images/dist3.pdf",width=7,height=5)
 dev.off()
 
