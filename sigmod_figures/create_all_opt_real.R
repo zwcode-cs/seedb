3af0f3e7d521a0eb8f836de10790b6967ac508e6
MV <mpvartak@gmail.com>
2015-02-27 21:59:40 -0500

    revamping expts

--- sigmod_figures/create_all_opt_real.R
+++ sigmod_figures/create_all_opt_real.R
@@ -9,7 +9,7 @@ all_opt_real <- function(filename) {
 	tmp$opt = ordered(tmp$opt, levels = c("NO_OPT", "SHARING", "SH_PR_FULL", "SH_PR_EARLY"))
         ggplot(tmp, aes(dataset, latency)) + 
 		geom_bar(aes(fill=opt), position="dodge", stat="identity") + 
-		ylab("latency (s)") +  theme_bw() + xlab("Datasets") + scale_y_log10() + 
+		ylab("latency (ms)") +  theme_bw() + xlab("Datasets") + scale_y_log10() + 
 		theme(text = element_text(size=24))  + scale_fill_brewer(palette="Paired");
 		ggsave(file=paste(outdirname, filename, ".pdf", sep=""),width=7,height=5);
 }
