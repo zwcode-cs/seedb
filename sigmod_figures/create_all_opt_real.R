a84db769cfdf0b0d51568533512e1ed47ac0de34
MV <mpvartak@gmail.com>
2015-02-28 11:19:37 -0500

    new charts

--- sigmod_figures/create_all_opt_real.R
+++ sigmod_figures/create_all_opt_real.R
@@ -6,12 +6,15 @@ all_opt_real2 <- function(filename) {
 	tmp$dataset = factor(tmp$dataset);
 	tmp$dataset = ordered(tmp$dataset, levels = c("BANK", "DIAB", "AIR", "AIR10"));
 	tmp$opt = factor(tmp$opt)
-	tmp$opt = ordered(tmp$opt, levels = c("NO_OPT", "SHARING", "SH_PR_FULL", "SH_PR_EARLY"))
+	tmp$opt = ordered(tmp$opt, levels = c("NO_OPT", "SHARING", "COMB", "COMB_EARLY"))
     ggplot(tmp, aes(opt, latency)) + 
 		geom_bar(aes(fill=opt), position="dodge", stat="identity") + 
-		ylab("latency (ms)") +  theme_bw() + xlab("Datasets") + scale_y_log10() + facet_grid(. ~ dataset) +
-		theme(axis.text.x = element_blank(), axis.ticks = element_blank()) +
-		theme(text = element_text(size=24))  + scale_fill_brewer(palette="Paired");
+		ylab("latency (ms)") +  theme_bw() + xlab("Datasets") + scale_y_log10(labels = trans_format("log10", math_format(10^.x))) +
+    facet_grid(. ~ dataset) +
+		theme(axis.text.x = element_blank(), axis.ticks = element_blank()) + theme(legend.position="top") +
+		theme(legend.title = element_text(size=16)) + theme(legend.text = element_text(size=16)) +
+		theme(text = element_text(size=20))  + scale_fill_brewer(palette="Paired") + 
+    geom_hline(yintercept=1000, linetype="dashed") + geom_text(aes(2,1000,label = "1s", vjust = -1));
 		ggsave(file=paste(outdirname, filename, ".pdf", sep=""),width=7,height=5);
 }
 
@@ -25,23 +28,82 @@ all_opt_real <- function(filename) {
 	tmp$opt = factor(tmp$opt)
 	tmp$opt = ordered(tmp$opt, levels = c("NO_OPT", "SHARING", "COMB", "COMB_EARLY"))
     
+    #p1 = helper(tmp, "BANK", outdirname, filename);
+    #p2 = helper(tmp, "DIAB", outdirname, filename);
+    #p3 = helper(tmp, "AIR", outdirname, filename);
+    #p4 = helper(tmp, "AIR10", outdirname, filename);
+    #multiplot(p1, p2, p3, p4, cols=4);
     helper(tmp, "BANK", outdirname, filename);
     helper(tmp, "DIAB", outdirname, filename);
     helper(tmp, "AIR", outdirname, filename);
     helper(tmp, "AIR10", outdirname, filename);
-    #multiplot(p1, p2, cols=2);
 }
 
 helper <- function(tmp, dataset, outdirname, filename) {
 	tmp = tmp[tmp$dataset==dataset,];
-	tmp
+	ylabel = "";
+	if (dataset == "BANK") {
+		ylabel = "latency (s)";
+	}
+
 	p = ggplot(tmp, aes(opt, latency/1000)) + 
-		geom_bar(aes(fill=opt), position="dodge", stat="identity") + guides(fill=FALSE) +
-		ylab("latency (s)") +  theme_bw() + xlab("") + #scale_y_log10() +
-		theme(text = element_text(size=24))  + scale_fill_brewer(palette="Paired");
-		ggsave(file=paste(outdirname, filename, "_", dataset, ".pdf", sep=""),width=7,height=5);
-	p;
+		geom_bar(aes(fill=opt), position="dodge", stat="identity") +
+		ylab(ylabel) +  theme_bw() + xlab("") + #scale_y_log10() +
+		theme(axis.text.x = element_blank(), axis.ticks = element_blank()) +
+		theme(text = element_text(size=12))  + scale_fill_brewer(palette="Paired");
+	if (dataset != "AIR10") {
+		p = p + guides(fill=FALSE);
+	}
+	ggsave(file=paste(outdirname, filename, "_", dataset, ".pdf", sep=""),width=7,height=7);
+	#p;
 }
 
-all_opt_real("all_opt_real_data_row");
-all_opt_real("all_opt_real_data_col");
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+library('scales');
+all_opt_real2("all_opt_real_data_row");
+all_opt_real2("all_opt_real_data_col");
